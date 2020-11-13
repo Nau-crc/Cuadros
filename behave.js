07bb1355753ea555edd2ff230f9ee5adb170792b
Laura <laura.rodcarrrion@gmail.com>
2020-11-13 15:02:37 +0100

    lista de cuadrados

---
+++ behave.js
@@ -0,0 +1,26 @@
+let comidasLatinoamericanas = ["Gallo Pinto","Arepas","Pati","Chorreadas", "Patacones","Casado"]
+    window.addEventListener('load',( => {
+        let canvasCuadrado = document.getElementById('Cuadrado');
+        let ctx = canvasCuadrado.getContext (2d);
+        ctx.fillstyle = '#FF0000';
+       
+    })
+let container
+    #container {
+        width: 400px;
+        height: 400px;
+        position: relative;
+        background: red;
+      }
+      #animate {
+        width: 50px;
+        height: 50px;
+        position: absolute;
+        background: blue;
+      }
+/*let comidasEspañolas = ["Paella","Tapas","Tortilla de Patatas","Callos a la Madrileña","Fricando","Caracoles a la Llauna"];
+    var elemento = document.getElementById("ComidasEspañolas");
+    var contexto = elemento.getContext('2d');
+    contexto.fillRect(100, 100, 100, 100);
+    ;*/
+
